c5e0e5565423bb85046147f4119a094b8f72b144
Andrew McAfee <andy@Andrews-MacBook-Pro-5.local>
2015-12-05 22:22:22 -0500

    Switching to Unicorn

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ActiveRecord::Base
-	has_attached_file :image, styles: {large:"800", medium:"600", small:"300", thumb:"100"}
+	has_attached_file :image, styles: {large:"800", medium:"600", small:"300", thumb:"100"},
+	:default_url => "missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	validates_attachment :image, size: { in:0..800.kilobytes }
