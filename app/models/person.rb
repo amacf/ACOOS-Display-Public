7e249e1499ed3299a442dd9d71f5d2bf1c8510d7
Andrew McAfee <andy@Andrews-MacBook-Pro-5.local>
2015-12-13 14:59:54 -0500

    Committing changes from 12/07 notes

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ActiveRecord::Base
 	has_attached_file :image, styles: {large:"800", medium:"450", small:"300", thumb:"100"},
-	:default_url => "missing.png"
+	:default_url => "missingPerson/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	validates_attachment :image, size: { in:0..800.kilobytes }
